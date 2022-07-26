ef976f68fabb5ce1cb887346f1b7d35199411228
LeonardoPadre <leonardopadre@hotmail.com>
2022-07-26 19:11:30 -0300

    Adicionando conexao com mySQL e Cadastro de dados na tabela

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const db = require('../models/db');
+const User = require('../models/User');
+
+app.use(express.json());
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -10,5 +14,22 @@ app.get("/", (req, res) => {
         message: "GFour Invext"
     });
 });
+
+app.post("/cadastrar", async (req, res) => {
+    console.log(req.body);
+
+    await User.create(req.body)
+    .then(() => {
+        return res.json({
+            erro: false,
+            message: "Usuario cadastrado com sucesso"
+        })
+    }).catch(() => {
+        return res.status(400).json({
+            erro: true,
+            message: "Usuario nao cadastrado com sucesso"
+        })
+    });
+})
 
 module.exports = app;
