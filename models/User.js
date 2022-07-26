b304491fd9a8dd55d48c8c3d5a05521e5ed7906e
LeonardoPadre <leonardopadre@hotmail.com>
2022-07-26 19:00:29 -0300

    Adicionando conexao com mySQL e Cadastro de dados na tabela

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+const db = require('./db');
+
+const User = db.define('users', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    senha: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    }
+});
+
+User.sync()
+
+module.exports = User;
