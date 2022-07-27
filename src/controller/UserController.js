f130bfab829d6c3fd2c0b3c1f622bc9603b90704
Lucas Gomes <lggomes007@gmail.com>
2022-07-27 18:51:15 -0300

    Adicionando somente o escopo do crud de usuarios

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -3,6 +3,7 @@ const app = express();
 
 const User = require('../models/User');
 
+// so esse funciona
 app.post("/cadastrar", async (req, res) => {
     console.log(req.body);
 
@@ -16,6 +17,60 @@ app.post("/cadastrar", async (req, res) => {
         return res.status(400).json({
             erro: true,
             message: "Usuario nao cadastrado com sucesso"
+        })
+    });
+})
+
+// nao funciona
+app.get("/buscar", async (req, res) => {
+    console.log(req.body);
+
+    await User.create(req.body)
+    .then(() => {
+        return res.json({
+            erro: false,
+            message: "Usuario cadastrado com sucesso"
+        })
+    }).catch(() => {
+        return res.status(400).json({
+            erro: true,
+            message: "Usuario nao cadastrado com sucesso"
+        })
+    });
+})
+
+// nao funciona
+app.post("/atualizar", async (req, res) => {
+    console.log(req.body);
+
+    await User.destroy(req.body)
+    .then(() => {
+        return res.json({
+            erro: false,
+            message: "Usuario deletado com sucesso"
+        })
+    }).catch(() => {
+        return res.status(400).json({
+            erro: true,
+            message: "Usuario nao deletado com sucesso"
+        })
+    });
+})
+
+// nao funciona
+app.post("/deletar", async (req, res) => {
+    console.log(req.body);
+
+    await User.destroy(req.body)
+    .then(() => {
+        return res.json({
+            erro: false,
+            message: "Usuario deletado com sucesso"
+        })
+    }).catch(() => {
+        return res.status(400).json({
+            erro: true,
+            message: "Usuario nao deletado com sucesso"
         })
     });
 })
