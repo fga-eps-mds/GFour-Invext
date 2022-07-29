c3277adadd79778e7d4414cdad69b069b7ba0ce9
Lucas Gomes <lggomes007@gmail.com>
2022-07-29 18:15:25 -0300

    Adicionando sitema de login com jwt

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+
 const User = require("../models/User");
 const app = express();
 
-// so esse funciona
+// funciona
 app.post("/cadastrar", async (req, res) => {
     const salt = await bcrypt.genSalt(10);
-    var usr = {
+    
+    const novo_usuario = {
         nomeCompleto: req.body.nomeCompleto,
         dataNascimento: req.body.dataNascimento,
         telefone: req.body.telefone,
@@ -14,56 +17,64 @@ app.post("/cadastrar", async (req, res) => {
         senha: await bcrypt.hash(req.body.senha, salt)
     };
 
-    const achou = await User.findOne({ where: { email: usr.email}  });
+    const usuario = await User.findOne({ where: { email: novo_usuario.email}  });
 
-    if (achou == null){
-        await User.create(usr)
+    if (usuario === null){
+        await User.create(novo_usuario)
         .then(() => {
             return res.json({
                 erro: false,
-                message: "Usuario cadastrado com sucesso"
+                message: "Usuario cadastrado com sucesso!"
             })
         }).catch(() => {
             return res.status(400).json({
                 erro: true,
-                message: "Usuario nao cadastrado com sucesso"
+                message: "Usuario nao cadastrado com sucesso!"
             })
         });
     } else {
         return res.status(400).json({
             erro: true,
-            message: "Usuario ja existente no banco"
+            message: "Usuario ja existente no banco!"
         })
     }
 })
 
-// nao funciona
+// funciona
 app.post("/login", async (req, res) => {
-    const user = await User.findOne({
+    const usuario = await User.findOne({
+        attributes: ["id", "email", "senha"],
         where: {
             email: req.body.email
         }
     });
-    if(user){
-        const validador_de_senha = await bcrypt.compare(req.body.senha, User.senha);
-        if(validador_de_senha){
-            return res.json({
-                erro: false,
-                message: "Usuario logado com sucesso"
-            })
-        } else {
-            return res.status(400).json({
-                erro: true,
-                message: "Usuario nao logado com sucesso"
-            })
-        }
-    } else {
+
+    if(usuario === null){
         return res.status(400).json({
             erro: true,
-            message: "Usuario nao existe"
-        })
+            message: "Usuario nao encontrado!"
+        });
+    } else {
+        if(!(await bcrypt.compare(req.body.senha, usuario.senha))){
+            return res.status(400).json({
+                erro: true,
+                mensagem: "Senha incorreta!"
+            });
+        } else {
+            const token = jwt.sign({id: usuario.id}, "INVEXTGFOURD62ST92Y7A6V7K5C6W9ZU6W8KS3", {
+                // expiresIn: 600 //10 min
+                // expiresIn: '7d' // 7 dia
+                expiresIn: 1800 //30 min
+            });
+        
+            return res.json({
+                erro: false,
+                mensagem: "Login realizado com sucesso!",
+                token
+            });
+        }
     }
-})
+});
 
 // nao funciona
 app.post("/atualizar", async (req, res) => {
