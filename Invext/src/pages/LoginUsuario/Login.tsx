f9cd77927b7936c431d9bbb552a8fb57fe0d35d7
Luciano Freitas <88516249+luciano-freitas-melo@users.noreply.github.com>
2022-07-29 20:29:55 -0300

    Revisões no Login.tsx
    - Removido imports que não estão sendo usados
    - Body em volta do código não é necessário, por isso foi removido sem problemas
    - Icone de voltar no login não é necessário, pois já tem o botão de inscreva-se
    - Trocado nome "div-cadastro" para "div-login" pois fica mais significativo
    - Trocado nome de "form-cadastro" para "form-login", pelo mesmo motivo do acima
    - Colocado tipos nos butões, pois o botão de increva-se não dava o event sem o navegador reclamar do formulário

--- Invext/src/pages/LoginUsuario/Login.tsx
+++ Invext/src/pages/LoginUsuario/Login.tsx
@@ -1,7 +1,5 @@
-import { IMaskInput } from "react-imask";
-import { IoMdArrowBack } from 'react-icons/io';
 import './Login.css';
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const LoginUsuario = () => {
 
@@ -32,40 +30,36 @@ const LoginUsuario = () => {
     }
            
         return (
-            <body>
-                <div className="background-img">
-                    <div className="div-cadastro">
-                        <IoMdArrowBack
-                            title="Voltar ao menu principal"
-                            className="icone-voltar"
+            
+            <div className="background-img">
+                <div className="div-login">
+                
+                    <h1 className="titulo">Entrar</h1>
+
+                    <form onSubmit={handleSubmit} className="form-login">
+                        <input
+                            type="email"
+                            name="email"
+                            required
+                            placeholder="E-mail"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)} 
                         />
-                        <h1 className="titulo">Entrar</h1>
 
-                        <form onSubmit={handleSubmit} className="form-cadastro">
-                            <input
-                                type="email"
-                                name="email"
-                                required
-                                placeholder="E-mail"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)} 
-                            />
-
-                            <input
-                                type="password"
-                                name="password"
-                                required
-                                placeholder="Senha "
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                            />                        
-                            <button className='submit-button'>Entrar</button>
-                            <button className='sign-up-button'>Inscreva-se</button>
-                            {error && <p className="error"> {error}</p>}
-                        </form>
-                    </div>
-                </div >
-            </body>
+                        <input
+                            type="password"
+                            name="password"
+                            required
+                            placeholder="Senha"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />                        
+                        <button type="submit" className='submit-button'>Entrar</button>
+                        <button type="button" className='sign-up-button'>Inscreva-se</button>
+                        {error && <p className="error"> {error}</p>}
+                    </form>
+                </div>
+            </div >
 
         
 
