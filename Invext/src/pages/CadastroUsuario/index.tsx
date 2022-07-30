a39a093c39fba76ce5de86a54a2e07a34ef05ce9
luciano-freitas-melo <202016847@aluno.unb.br>
2022-07-29 23:05:17 -0300

    Validações dos inputs data e nome

--- Invext/src/pages/CadastroUsuario/index.tsx
+++ Invext/src/pages/CadastroUsuario/index.tsx
@@ -1,7 +1,8 @@
 import { IMaskInput } from "react-imask";
 import { IoMdArrowBack } from 'react-icons/io';
 import './styles.css';
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { parseISO, isAfter, sub, add } from "date-fns";
 
 const CadastroUsuario = () => {
 
@@ -13,7 +14,8 @@ const CadastroUsuario = () => {
     const [confirmPassword, setConfirmPassword] = useState("");
     const [error, setError] = useState("");
 
-    const handleSubmit = async (e : React.FormEvent<HTMLFormElement>) => {
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
         setError("");
@@ -23,21 +25,40 @@ const CadastroUsuario = () => {
             email,
             password,
             phone,
-            birth,
+            birth
         }
 
-        if (password != confirmPassword) {
-            setError("As senhas precisam ser iguais");
-    
+        // Transforma a data de nascimento em um objeto Date
+        const parsedBirth = parseISO(birth);
+
+        // Verifica se a pessoa tem menos de 18 anos
+        if (isAfter(parsedBirth, sub(new Date(), { years: 18 }))) {
+            setError("É necessário ter mais de 18 anos!");
+
+            // Verifica se a pessoa colocou uma data de nasc. aceitável (tem menos de 120 anos)
+        } else if (isAfter(parsedBirth, add(new Date(), { years: 120 }))) {
+            setError("Insira uma data de nascimento válida!");
+
         } else if (password.length < 8) {
             setError("A senha precisa ter pelo menos 8 caracteres");
-    
+
+        } else if (password != confirmPassword) {
+            setError("As senhas precisam ser iguais");
+
         } else {
             setError("");
         };
     }
 
-   
+
+    // Função para validar se o input do nome se encaixa na regex(somente letras)
+    const nameMask = function (value: string) {
+        var pattern = new RegExp(/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/);
+        return pattern.test(value);
+    };
+
+    /* State para alterar o tipo do input Data */
+    const [inputType, setInputType] = useState("text");
 
 
     return (
@@ -52,22 +73,26 @@ const CadastroUsuario = () => {
 
                     <form onSubmit={handleSubmit} className="form-cadastro">
 
-                        <input
-                            type="text"
+                        <IMaskInput
+                            mask={nameMask}
                             name="displayName"
-                            required placeholder="Nome Completo"
+                            required
+                            placeholder="Nome Completo"
                             value={displayName}
-                            onChange={(e) => setDisplayName(e.target.value)}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                                setDisplayName(e.currentTarget.value)}
                         />
 
-                        <IMaskInput
-                            type="text"
+                        <input
+                            placeholder="Data de Nascimento"
+                            type={inputType}
                             name="birth"
                             required
-                            mask="00/00/0000"
-                            placeholder="Data de Nascimento"
                             value={birth}
-                            onChange={(e) => setBirth(e.currentTarget.value)}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                                setBirth(e.currentTarget.value)}
+                            onFocus={() => setInputType("date")}
+                            onBlur={() => setInputType("text")}
                         />
 
                         <IMaskInput
@@ -76,7 +101,8 @@ const CadastroUsuario = () => {
                             mask="(00) 00000-0000"
                             placeholder="Telefone (opcional)"
                             value={phone}
-                            onChange={(e) => setPhone(e.currentTarget.value)}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => 
+                                setPhone(e.currentTarget.value)}
                         />
 
                         <input
@@ -85,7 +111,8 @@ const CadastroUsuario = () => {
                             required
                             placeholder="E-mail"
                             value={email}
-                            onChange={(e) => setEmail(e.target.value)} 
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => 
+                                setEmail(e.target.value)}
                         />
 
                         <input
@@ -94,7 +121,8 @@ const CadastroUsuario = () => {
                             required
                             placeholder="Senha "
                             value={password}
-                            onChange={(e) => setPassword(e.target.value)}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => 
+                                setPassword(e.target.value)}
                         />
 
                         <input
@@ -103,9 +131,10 @@ const CadastroUsuario = () => {
                             required
                             placeholder="Confirme a senha "
                             value={confirmPassword}
-                            onChange={(e) => setConfirmPassword(e.target.value)}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => 
+                                setConfirmPassword(e.target.value)}
                         />
-                        
+
                         <button className='submit-button'>Cadastrar</button>
                         {error && <p className="error"> {error}</p>}
                     </form>
