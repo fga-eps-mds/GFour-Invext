c38170452f1b97e589d25dd955e6114066c0e9c5
luciano-freitas-melo <202016847@aluno.unb.br>
2022-08-01 19:54:53 -0300

    Link das rotas de cadastro

--- Invext/src/App.tsx
+++ Invext/src/App.tsx
@@ -1,18 +1,18 @@
 import './App.css'
-import CadastroUsuario from './pages/CadastroUsuario'
+import CadastroUsuario from './pages/CadastroUsuario/Cadastro'
 import LoginUsuario from './pages/LoginUsuario/Login'
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
 
   return (
     <div className="App">
-          <Router>
-            <Routes>
-              <Route path = "/" element = {<LoginUsuario/>}/>
-              <Route path = "/cadastro" element = {<CadastroUsuario/>}/>
-            </Routes>
-          </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginUsuario />} />
+          <Route path="/cadastro" element={<CadastroUsuario />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
