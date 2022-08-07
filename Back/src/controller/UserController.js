e69a8f2e65a78e433c54d1a9ced1036ec029a789
Josué Teixeira <zjosuez@outlook.com>
2022-08-07 18:10:21 -0300

    Revert "adicionado escopo do banco de dados da ação"
    This reverts commit 40b00ba08fb44df14f5e0e14597a5490687fb45b.

--- Back/src/controller/UserController.js
+++ Back/src/controller/UserController.js
@@ -3,7 +3,6 @@ const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
 
 const User = require("../models/User");
-const Acoes = require("../models/Acao");
 const app = express();
 
 // funciona
