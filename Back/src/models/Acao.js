34b28534ccded78f5ca16437998af4e3d49f06f6
Lucas Gomes <lggomes007@gmail.com>
2022-08-07 23:17:16 -0300

    Configurancao tabela de acao no banco de dados

---
+++ Back/src/models/Acao.js
@@ -0,0 +1,58 @@
+const Sequelize = require('sequelize');
+const db = require('./db');
+
+const Acao = db.define('acao', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    nomeAtivo: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: "Esse campo nao pode ser vazio"
+            }
+        }
+    },
+    sigla: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: "Esse campo nao pode ser vazio"
+            }
+        }
+    },
+    preco: {
+        type: Sequelize.FLOAT,
+        allowNull: true,
+    },
+    quantidade: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: "Esse campo nao pode ser vazio"
+            }
+        }
+    },
+    dataCompra: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: "Esse campo nao pode ser vazio"
+            }
+        }
+    }
+});
+
+Acao.sync()
+
+// verifica se existe alteração na model que não está no BD
+// Acao.sync({ alter: true })
+
+module.exports = Acao;
