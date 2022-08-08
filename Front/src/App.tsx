4a1ff3b28f6ed69de26dc8748a04c9b2d437f045
luciano-freitas-melo <202016847@aluno.unb.br>
2022-08-08 18:15:09 -0300

    Configuração das rotas no App e na Main

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,18 +1,33 @@
 import './App.css'
 import CadastroUsuario from './pages/CadastroUsuario/Cadastro'
 import LoginUsuario from './pages/LoginUsuario/Login'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {Route, Routes } from "react-router-dom";
+import { AuthProvider } from './services/Provider';
+import { RequireAuth } from './services/requireAuth';
+
+// Remover e colocar a side bar que realmente será implementada
+function SideBar() {
+  return <div>SideBar</div>
+}
 
 function App() {
 
   return (
     <div className="App">
-      <Router>
+      <AuthProvider>
         <Routes>
           <Route path="/" element={<LoginUsuario />} />
           <Route path="/cadastro" element={<CadastroUsuario />} />
+
+          <Route path="/index" element={
+            <RequireAuth>
+              <SideBar />
+            </RequireAuth>
+          } >
+            {/* Aqui ficarão as outras rotas do usuário logado */}
+          </Route>
         </Routes>
-      </Router>
+      </AuthProvider>
     </div>
   )
 }
