014912a5829b2db18907b82b572312557930c51e
amanda-noda <amanda.fnodaa@gmail.com>
2022-08-21 22:25:21 -0300

    Cria tabela na tela de histórico.

--- Front/src/pages/HistoricoDeAcoes/historico.tsx
+++ Front/src/pages/HistoricoDeAcoes/historico.tsx
@@ -1,4 +1,7 @@
+import { Link } from 'react-router-dom';
 import './historico.css';
+var novaTabela = document.createElement("table");
+
 
 
 const HistoricoDeAcoes = () => {
@@ -14,7 +17,43 @@ return (
         <div className="background-img">        
             <h1 className='titulo'>Invext - Histórico de ações  </h1>
             <div className="div-historico">
-                <ul>{listItems}</ul>
+                <div id="test">
+                <Link to='/historico'/>  
+                    <table>
+                    <thead>
+                    <tr>
+                    <th>Ativo</th>
+                    <th>Sigla</th>
+                    <th>Ordem</th>
+                    <th>Quantidade</th>
+                    <th>Valor</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>    
+                    <td>--</td>
+                    <td>--</td>
+                    <td>--</td>
+                    <td>--</td>
+                    <td>--</td>
+                    </tr>
+                    <tr>
+                    <td>--</td>
+                    <td>--</td>
+                    <td>--</td>
+                    <td>--</td>
+                    <td>--</td>
+                    </tr>
+                    <tr>
+                    <td>--</td>
+                    <td>--</td>
+                    <td>--</td>
+                    <td>--</td>
+                    <td>--</td>
+                    </tr>
+                    </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </body>
