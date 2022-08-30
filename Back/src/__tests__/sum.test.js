9c5d8f974f4d203bff41a9e9e2efea214d94b5b4
Lucas Felipe <lucasfs1007@gmail.com>
2022-08-30 17:30:46 -0300

    Merge branch '81-84_Backend' into Develop

---
+++ Back/src/__tests__/sum.test.js
@@ -0,0 +1,13 @@
+describe('sum test', () => {
+    it('should sum to numbers', () => {
+    const a = 1, b = 2;
+    const c = a + b;
+    expect(c).toBe(3);
+    });
+    
+    it('should sum to numbers',() => {
+    const a = 1, b = 2;
+    const c = a + b;
+    expect(c).toBe(3);
+    });
+    });
--- Back/src/util/AtivosB3Util.js
+++ Back/src/util/AtivosB3Util.js
@@ -16,19 +16,25 @@ exports.updateAtivosB3 = async function () {
     // Seleciona todas as informacoes disponiveis sobre todos os ativos nesse dia
     // e atualiza o banco de dados com as novas contacoes do dia
     await Axios.get(`https://api-cotacao-b3.labdo.it/api/cotacao/dt/${dt_ultimo_pregao}/02`, {
-    }).then(function(res){
+    }).then(async function(res){
         const { data } = res;
-
-        for (let ativo of data) {
-            const novo_ativo = {
-                data_pregao: ativo.dt_pregao,
-                codigo_acao: ativo.cd_acao,
-                nome_empresa: ativo.nm_empresa_rdz,
-                valor_fechamento: ativo.vl_fechamento
+        var lista = [];
+        // deleta todos os registros do banco primeiro
+        await AtivosB3.destroy({
+            where: {},
+            truncate: true
+        }).then(async () => {
+            for (let ativo of data) {
+                const novo_ativo = {
+                    data_pregao: ativo.dt_pregao,
+                    codigo_acao: ativo.cd_acao,
+                    nome_empresa: ativo.nm_empresa_rdz,
+                    valor_fechamento: ativo.vl_fechamento
+                }
+                lista.push(novo_ativo);
             }
-
-            AtivosB3.create(novo_ativo); // adiciona o novo ativo no banco a cada iteracao
-        }
+            await AtivosB3.bulkCreate(lista); // adiciona todos os ativos no banco de uma vez
+        });
     }).catch(function(err){
         console.log(err);
     });
