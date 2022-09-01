811e7b4aa728effe21f30fc4e68d1eb23006b934
Lucas Felipe <lucasfs1007@gmail.com>
2022-09-01 17:44:41 -0300

    inicializando a integração da carteira com o back

--- Front/src/pages/CarteiraUsuario/Carteira.tsx
+++ Front/src/pages/CarteiraUsuario/Carteira.tsx
@@ -1,14 +1,77 @@
-//Amanda disse que vai implementar o esquema de tabela p dentro da div ver com ela dps.
-//O CSS vai ter que ser um pouco refatorado pois estamos usando muito em e isso pode dar ruim dependendo da tela.
 import './Carteira.css';
-import { useState } from "react"; 
+import { useState,useEffect } from "react"; 
 import {FaMoneyBillWave} from 'react-icons/fa';
 import {BsEye, BsEyeSlash} from 'react-icons/bs';
 import {AiOutlineRise} from 'react-icons/ai'
+import Axios from 'axios';
+//preciso verificar todas as importaçoes restantes e corrigir essa importação
+import { DataGrid, GridColDef} from '@mui/x-data-grid';
+import { useAuth } from '../../services/Provider';
 
-const Carteira = () => {
-    
-   
+//base da tabela...estou usando tudo que tava no prototipo
+const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 35 },
+    { field: 'acoes', headerName: 'Ações', width: 65 },
+    { field: 'sigla', headerName: 'Sigla', width: 65 },
+    { field: 'porc', headerName: 'Porc.', width: 65 },
+    { field: 'quantidade', headerName: 'Quantidade', width: 65 },
+    { field: 'precoAtual', headerName: 'Preço atual', width: 65 },
+    { field: 'precoMedio', headerName: 'Preço médio', width: 65 },
+    { field: 'diferenca', headerName: 'Diferença', width: 65 },
+    { field: 'valorTotal', headerName: 'Valor total', width: 65 },
+  
+  ];
+  
+  // São os registros que vem do banco de dados
+  interface DBHistoric { //trouxe da tela de historico
+    id: number,
+    nomeAtivo: string,
+    sigla: string,
+    execucao: string,
+    quantidade: number,
+    data: string,
+    preco: number
+  }
+
+  //passo tudo que esta em field para essa interface...verificar se esta ok os nomes e os tipos.
+  interface Carteir {
+  id: number,
+  ativo: string, 
+  sigla: string,
+  porc:number,
+  quantidade: number, 
+  precoAtual: number,
+  precoMedio: number, 
+  diferenca: number, 
+  valorTotal:number,
+  }
+
+
+
+export const Carteira = () => {
+
+    const auth = useAuth();
+    const token = auth.getToken();
+    const [carteir, setCarteir] = useState<Carteir[]>();
+
+    //vamos ter que mexer na forma como esses dados vem do banco...estou um pouco incerto dessa parte
+
+    const getCarteir = () => {
+        //estou puxando os mesmos dados do historico
+        Axios.post("/ativo/historico",{ 
+         token: token
+        }
+         ).then(function(response){
+           setCarteir(response.data.historico);
+         }).catch(function (error) {
+           console.log(error);
+       })
+     }
+
+     useEffect(() => {
+        getCarteir();
+      }, [ ])
+
 return (
     <div className='background-img-carteira'>
         <h1 className="titulo-carteira">Invext - Minha Carteira</h1>
@@ -21,6 +84,13 @@ return (
 
             </div>
             <div className='div-ativos-patrimonio'>
+            <DataGrid
+            rows={carteir}
+            columns={columns}
+            pageSize={10}
+            rowsPerPageOptions={[10]}
+            checkboxSelection
+          />
                 <span className='barra-patrimonio'></span>
                 
                 
