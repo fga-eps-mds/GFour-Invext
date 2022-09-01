6df50f9ba1260f7ff1a575043448625e99823c74
Josué Teixeira <zjosuez@outlook.com>
2022-09-01 16:23:42 -0300

    teste: implementando novos testes Co-authored-by: Maria Abritta <MariaAbritta@users.noreply.github.com>

---
+++ Back/src/__tests__/auth.test.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    try {
+        const token = null;
+        const decode = jwt.verify(token, "INVEXTGFOURD62ST92Y7A6V7K5C6W9ZU6W8KS3");
+        req.usuario = decode;
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            erro: true,
+            message: "Falha na autenticação!"
+        });
+    }
+}
---
+++ Back/src/__tests__/usuario.test.js
@@ -0,0 +1,95 @@
+const request = require("supertest");
+const { response } = require("../app");
+const app = require("../app");
+const db = require("../models/db");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+describe("Testes de usuario", () => {
+  beforeEach(async () => {
+    console.log("Iniciando os Testes");
+  });
+  afterAll(async () => {
+    db.close();
+  });
+  
+  it("E possivel criar um novo usuario", async () => {
+
+    const novo_usuario = {
+      nomeCompleto: "asd",
+      dataNascimento: "02/02/2002",
+      telefone: "61998753416",
+      email: "asd22@gmail.com",
+      senha: "123456789",
+  }
+  const novo_usuario2 = {
+    nomeCompleto: "asd",
+    dataNascimento: "02/02/2002",
+    telefone: "61998753416",
+    email: "asd21@gmail.com",
+    senha: "123456789",
+}
+    
+    const response = await request(app).post("/usuario/cadastrar").send({
+      nomeCompleto: novo_usuario.nomeCompleto,
+      dataNascimento: novo_usuario.dataNascimento,
+      telefone: novo_usuario.telefone,
+      email: novo_usuario.email,
+      senha: novo_usuario.senha,
+    });
+
+    const response2 = await request(app).post("/usuario/cadastrar").send({
+        nomeCompleto: novo_usuario2.nomeCompleto,
+        dataNascimento: novo_usuario2.dataNascimento,
+        telefone: novo_usuario2.telefone,
+        email: novo_usuario2.email,
+        senha: novo_usuario2.senha,
+      });
+
+    // expect(response.status).toBe(200);
+
+    const usuario = await User.findOne({ where: { email: novo_usuario.email}  });
+
+    if (usuario === null){
+        await User.create(novo_usuario)
+        .then(() => {
+            return res.json({
+                erro: false,
+                message: "Usuario cadastrado com sucesso!"
+            }).expect.assertions(1)
+        }).catch((error) => {
+            expect(res.json.status).toBe(400);
+            // done(error);
+        });
+    }
+
+  });
+
+  it("E possivel Logar", async () => {
+
+    const usuario = await User.findOne({
+      attributes: ["id", "email", "senha"],
+      where: {
+          email: "asd7@gmail.com",
+         
+      }
+  });
+    const senha = await bcrypt.getSalt(usuario.senha);
+    const response2 = await request(app).post("/usuario/login").send({
+  
+      email: usuario.email,
+      senha: senha,
+    });
+
+    const usuario2 = null;
+    const senha2 = await bcrypt.getSalt(usuario.senha);
+    const response3 = await request(app).post("/usuario/login").send({
+  
+      email: usuario2,
+      senha: senha2,
+    });
+    
+
+  });
+  
+});
