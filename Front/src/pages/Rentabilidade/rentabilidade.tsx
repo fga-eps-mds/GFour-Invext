f6f22ab948d0be6f8a3dd79cff320bebf820816b
Lucas Felipe <lucasfs1007@gmail.com>
2022-09-02 16:21:03 -0300

    iniciando a base da estilização da tela de rentabilidade

--- Front/src/pages/Rentabilidade/rentabilidade.tsx
+++ Front/src/pages/Rentabilidade/rentabilidade.tsx
@@ -1,7 +1,19 @@
+import "./rentabilidade.css";
 
 
 export const Rentabilidade = () => {
     return(
-        <></>
+        <div className="background-img-rentabilidade">
+        <h1 className="titulo-rentabilidade">Rentabilidade</h1>
+        <div className="div-rentabilidade">
+          <span className="barra-rentabilidade"></span>
+          <div className="div-ativos-rentabilidade">
+         
+          
+          <div className="div-grid-rentabilidade">
+              </div>
+          </div>
+        </div>
+      </div>
     );
 }
