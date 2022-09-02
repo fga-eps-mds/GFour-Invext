631cc80ab68eb4dc649e42fca1ce3729a9c9b71e
Lucas Felipe <lucasfs1007@gmail.com>
2022-09-02 17:41:09 -0300

    montando a base da tela de rentabilidade

--- Front/src/pages/Rentabilidade/rentabilidade.tsx
+++ Front/src/pages/Rentabilidade/rentabilidade.tsx
@@ -4,16 +4,11 @@ import "./rentabilidade.css";
 export const Rentabilidade = () => {
     return(
         <div className="background-img-rentabilidade">
-        <h1 className="titulo-rentabilidade">Rentabilidade</h1>
-        <div className="div-rentabilidade">
-          <span className="barra-rentabilidade"></span>
-          <div className="div-ativos-rentabilidade">
-         
-          
-          <div className="div-grid-rentabilidade">
-              </div>
+          <h1 className="titulo-rentabilidade">Rentabilidade</h1>
+          <div className="div-rentabilidade">
+             <div className="div-grid-rentabilidade">
+             </div>
           </div>
-        </div>
       </div>
     );
 }
