6475d4ce4d72c362df06fa400e41a72b0d112e4e
luciano-freitas-melo <202016847@aluno.unb.br>
2022-09-02 12:57:17 -0300

    feat: excluir registro de ativo

--- Front/src/pages/HistoricoDeAcoes/historico.tsx
+++ Front/src/pages/HistoricoDeAcoes/historico.tsx
@@ -1,12 +1,13 @@
 import './historico.css';
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
-import { DataGrid, GridColDef} from '@mui/x-data-grid';
+import { BiEdit } from 'react-icons/bi';
+import { RiDeleteBinLine } from 'react-icons/ri'
+import { DataGrid, GridActionsCellItem, GridColDef } from '@mui/x-data-grid';
 import { useAuth } from '../../services/Provider';
-import { useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import Axios from 'axios';
 import { capitalize } from '@mui/material';
 import { format } from 'date-fns';
+import { excluirAtivo } from './excluirAtivo';
 
 const columns: GridColDef[] = [
   { field: 'id', headerName: 'ID', width: 70 },
@@ -16,7 +17,28 @@ const columns: GridColDef[] = [
   { field: 'quantidade', headerName: 'Quantidade', width: 130 },
   { field: 'negociação', headerName: 'Negociação', width: 130 },
   { field: 'valor', headerName: 'Valor', width: 130 },
+  {
+  field: 'edit-column',
+    headerName: ' ',
+    sortable: false,
+    disableColumnMenu: true,
+    width: 80,
+    renderCell: (params) => [
+      <div key={params.id}>
+        <GridActionsCellItem
+          icon={<BiEdit />}
+          label='Editar'
+          onClick={() => null}
+        />
+        <GridActionsCellItem 
+          icon={<RiDeleteBinLine />}
+          label='Deletar'
+          onClick={() => excluirAtivo(params.id.toString())}
+        />
+      </div>
 
+    ],
+  },
 ];
 
 // São os registros que vem do banco de dados
@@ -42,58 +64,54 @@ interface Historic {
 }
 
 export const HistoricoDeAcoes = () => {
-  
+
   const auth = useAuth();
   const token = auth.getToken();
-  const [historic, setHistoric] = useState<Historic[]>(); 
+  const [historic, setHistoric] = useState<Historic[]>();
 
   // Altera a forma de visualização dos dados que vem do banco
   const refactorHistoric = (dbHistoric: DBHistoric[]) => {
-    if(dbHistoric){
-      return dbHistoric.map((ativo:DBHistoric) => ({
+    if (dbHistoric) {
+      return dbHistoric.map((ativo: DBHistoric) => ({
         id: ativo.id,
         ativo: ativo.nomeAtivo,
         sigla: ativo.sigla,
         ordem: capitalize(ativo.execucao),
         quantidade: ativo.quantidade,
         negociação: format(new Date(ativo.data), 'dd/MM/yyyy'),
-        valor: `R$ ${ativo.preco.toFixed(2)}` 
+        valor: `R$ ${ativo.preco.toFixed(2)}`
       }))
-     
-    } else{
+
+    } else {
       return [];
     }
   }
-
-  const getHistoric = () => {
-     Axios.post("/ativo/historico",{
-      token: token
-     }
-      ).then(function(response){
-        setHistoric(refactorHistoric(response.data.historico));
-      }).catch(function (error) {
-        console.log(error);
-    })
-  }
-
+  // Pega o historico do usuario quando o componente é renderizado
   useEffect(() => {
-    getHistoric();
-  }, [ ])
+    Axios.post("/ativo/historico", {
+      token: token
+    }
+    ).then(function (response) {
+      setHistoric(refactorHistoric(response.data.historico));
+    }).catch(function (error) {
+      console.log(error);
+    })
+  })
 
   return (
     <div className="background-img">
       <h1 className='titulo-historico'>Histórico de Ativos</h1>
       <div className="div-historico">
-        <div style={{ height: 400, width: '100%' }}> 
-          {historic ? 
-          <DataGrid
-          rows={historic}
-          columns={columns}
-          pageSize={10}
-          rowsPerPageOptions={[10]}
-          checkboxSelection
-          />
-          :null}
+        <div style={{ height: 400, width: '100%' }}>
+          {historic ?
+            <DataGrid
+              rows={historic}
+              columns={columns}
+              pageSize={10}
+              rowsPerPageOptions={[10]}
+              isRowSelectable={() => false}
+            />
+            : null}
         </div>
       </div>
     </div>
