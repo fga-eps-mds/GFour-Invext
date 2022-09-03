3258a3288387ab6998530e20e29f4bde93b38b75
luciano-freitas-melo <202016847@aluno.unb.br>
2022-09-02 21:14:05 -0300

    refactor: tabela de historico

--- Front/src/pages/HistoricoDeAcoes/historico.tsx
+++ Front/src/pages/HistoricoDeAcoes/historico.tsx
@@ -5,55 +5,14 @@ import { DataGrid, GridActionsCellItem, GridColDef } from '@mui/x-data-grid';
 import { useAuth } from '../../services/Provider';
 import { useEffect, useState } from 'react';
 import Axios from 'axios';
-import { capitalize } from '@mui/material';
 import { format } from 'date-fns';
 import { excluirAtivo } from './excluirAtivo';
+import { parse } from 'date-fns/esm';
 
-const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'ativo', headerName: 'Ativo', width: 130 },
-  { field: 'sigla', headerName: 'Sigla', width: 130 },
-  { field: 'ordem', headerName: 'Ordem', width: 130 },
-  { field: 'quantidade', headerName: 'Quantidade', width: 130 },
-  { field: 'negociação', headerName: 'Negociação', width: 130 },
-  { field: 'valor', headerName: 'Valor', width: 130 },
-  {
-  field: 'edit-column',
-    headerName: ' ',
-    sortable: false,
-    disableColumnMenu: true,
-    width: 80,
-    renderCell: (params) => [
-      <div key={params.id}>
-        <GridActionsCellItem
-          icon={<BiEdit />}
-          label='Editar'
-          onClick={() => null}
-        />
-        <GridActionsCellItem 
-          icon={<RiDeleteBinLine />}
-          label='Deletar'
-          onClick={() => excluirAtivo(params.id.toString())}
-        />
-      </div>
 
-    ],
-  },
-];
-
-// São os registros que vem do banco de dados
-interface DBHistoric {
-  id: number,
-  nomeAtivo: string,
-  sigla: string,
-  execucao: string,
-  quantidade: number,
-  data: string,
-  preco: number
-}
 
 // Registro que sera mostrado para o usuario
-interface Historic {
+interface Register {
   id: number,
   ativo: string, // == nomeAtivo
   sigla: string,
@@ -67,36 +26,72 @@ export const HistoricoDeAcoes = () => {
 
   const auth = useAuth();
   const token = auth.getToken();
-  const [historic, setHistoric] = useState<Historic[]>();
+  // Todo o historico de compra e venda de ativos
+  const [historic, setHistoric] = useState<Register[]>();
+  // Dita quando o historico deve ser puxado do backend
+  const [renderData, setRenderData] = useState(true);
 
-  // Altera a forma de visualização dos dados que vem do banco
-  const refactorHistoric = (dbHistoric: DBHistoric[]) => {
-    if (dbHistoric) {
-      return dbHistoric.map((ativo: DBHistoric) => ({
-        id: ativo.id,
-        ativo: ativo.nomeAtivo,
-        sigla: ativo.sigla,
-        ordem: capitalize(ativo.execucao),
-        quantidade: ativo.quantidade,
-        negociação: format(new Date(ativo.data), 'dd/MM/yyyy'),
-        valor: `R$ ${ativo.preco.toFixed(2)}`
-      }))
 
-    } else {
-      return [];
-    }
-  }
-  // Pega o historico do usuario quando o componente é renderizado
+  // Pega o historico do usuario do backend
   useEffect(() => {
-    Axios.post("/ativo/historico", {
-      token: token
+    if (renderData) {
+      setRenderData(false);
+      Axios.post("/ativo/historico", {
+        token: token
+      }
+      ).then(function (response) {
+        setHistoric(response.data.historico);
+      }).catch(function (error) {
+        console.log(error);
+      })
     }
-    ).then(function (response) {
-      setHistoric(refactorHistoric(response.data.historico));
-    }).catch(function (error) {
-      console.log(error);
-    })
-  })
+  },[renderData])
+
+
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'nomeAtivo', headerName: 'Ativo', width: 130 },
+    { field: 'sigla', headerName: 'Sigla', width: 130 },
+    { field: 'execucao', headerName: 'Ordem', width: 130 },
+    { field: 'quantidade', headerName: 'Quantidade', width: 130 },
+    {
+      field: 'data',
+      headerName: 'Negociação',
+      width: 130,
+      valueFormatter: (params) => format(parse(params.value,'yyyy-MM-dd',new Date),"dd/MM/yyyy")
+    },
+    { 
+      field: 'preco', 
+      type: 'number',
+      headerName: 'Valor', 
+      width: 130,
+      valueFormatter: (params) => `R$ ${params.value.toFixed(2)}`
+    },
+    {
+      field: 'edit-column',
+      headerName: ' ',
+      sortable: false,
+      disableColumnMenu: true,
+      width: 80,
+      renderCell: (params) => [
+        <div key={params.id}>
+          <GridActionsCellItem
+            icon={<BiEdit />}
+            label='Editar'
+            onClick={() => null}
+          />
+          <GridActionsCellItem
+            icon={<RiDeleteBinLine />}
+            label='Deletar'
+            onClick={() => {
+              excluirAtivo(params.id.toString());
+              setRenderData(true);
+            }}
+          />
+        </div>
+      ],
+    },
+  ];
 
   return (
     <div className="background-img">
