f0c33f7b9e308e056e8e6746cec73a69679c28fd
luciano-freitas-melo <202016847@aluno.unb.br>
2022-09-11 20:06:47 -0300

    refactor: editar ativo somente para qtd, preco e data

--- Front/src/components/PopoverEditarAtivo/editarAtivo.tsx
+++ Front/src/components/PopoverEditarAtivo/editarAtivo.tsx
@@ -1,15 +1,10 @@
-import { Dialog, DialogContent, Button, Select, MenuItem } from "@mui/material"
+import { Dialog, DialogContent, Button } from "@mui/material"
 import axios from "axios";
 import { useState } from "react";
 import { IMaskInput } from "react-imask";
 import { Register } from "../../pages/HistoricoDeAcoes/historico";
 import { returnToken } from "../../services/authToken";
-import { BuscaAtivo } from "../BuscaAtivos/Busca"
 import './editarAtivo.css'
-interface Assets {
-    nome: string,
-    sigla: string
-}
 
 interface Props {
     isOpen: boolean,
@@ -22,12 +17,10 @@ export const EditarAtivo = (p: Props) => {
     const token = returnToken();
     const [error, setError] = useState("");
 
-    const initialAssets: Assets = { nome: p.initialValues.nomeAtivo, sigla: p.initialValues.sigla };
-    const [assets, setAssets] = useState<Assets>(initialAssets); // É o ativo
     const [stockPrice, setStockPrice] = useState(p.initialValues.preco.toString()); //preço das ações
     const [date, setDate] = useState(p.initialValues.data);
     const [quantity, setQuantity] = useState(p.initialValues.quantidade.toString());
-    const [type, setType] = useState(p.initialValues.execucao); // compra ou venda
+
     //para a formatar a data
     const [inputType, setInputType] = useState("text");
 
@@ -35,33 +28,29 @@ export const EditarAtivo = (p: Props) => {
         p.setIsOpen(false);
     }
 
-
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
         setError("");
-        if (!assets) {
-            setError("Selecione um ativo");
-
-        } else if (parseInt(quantity) <= 0) {
+        if (parseInt(quantity) <= 0) {
             setError("É necessário inserir uma quantidade válida");
 
         } else if (parseInt(stockPrice) <= 0) {
             setError("É necessário inserir um valor válido");
 
-        } else{
+        } else {
             axios.post("/ativo/editar",
                 {
                     token: token,
                     id: p.initialValues.id,
-                    sigla: assets.sigla,
+                    data: date,
                     preco: stockPrice,
                     quantidade: quantity
-        
+
                 }).then(function (response) {
                     alert(response.data.message);
                     p.callback();
-                    closeModal();   
+                    closeModal();
 
                 }).catch(function (error) {
                     const message = error.response.data.message;
@@ -75,18 +64,18 @@ export const EditarAtivo = (p: Props) => {
             <DialogContent>
                 <form onSubmit={handleSubmit} className='modalEdit'>
                     <h2>Editar Ativo</h2>
-                    <div className="busca-ativo">
-                        <BuscaAtivo
-                            value={assets!}
-                            setValue={setAssets}
-                        />
-                    </div>
+
+                    <input
+                        className="busca-ativo"
+                        disabled
+                        value={p.initialValues.nomeAtivo.concat(' - ', p.initialValues.sigla)}
+                    />
+
                     <div className="modalEdit-linebox">
                         <IMaskInput
                             mask={Number}
                             scale={2}
                             name="preco"
-                            max={999.99}
                             radix="."
                             mapToRadix={[',']}
                             padFractionalZeros={true}
@@ -120,15 +109,12 @@ export const EditarAtivo = (p: Props) => {
                             onFocus={() => setInputType("date")}
                             onBlur={() => setInputType("text")}
                         />
-                        <Select
-                            required
-                            value={type}
-                            onChange={(e) => setType(e.target.value)}
+                        <input
+                            disabled
+                            value={p.initialValues.execucao}
                             placeholder="Ordem"
-                        >
-                            <MenuItem value='compra'>Compra</MenuItem>
-                            <MenuItem value='venda'>Venda</MenuItem>
-                        </Select>
+                        />
+
                     </div>
                     <div className="buttonBox">
                         <Button type="button" onClick={closeModal}>Cancelar</Button>
