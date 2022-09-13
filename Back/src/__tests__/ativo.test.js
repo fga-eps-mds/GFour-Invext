7490e136382a6de94fb7a75a54b4037da8bd8a9e
Josué Teixeira <zjosuez@outlook.com>
2022-09-13 19:30:34 -0300

    teste: implementado novos testes

--- Back/src/__tests__/ativo.test.js
+++ Back/src/__tests__/ativo.test.js
@@ -3,6 +3,7 @@ const app = require("../app");
 const db = require("../models/db");
 const auth = require("../middleware/auth");
 
+
 describe("Teste dos ativos", () => {
   beforeEach(async () => {
     console.log("Iniciando os Testes");
@@ -18,58 +19,94 @@ describe("Teste dos ativos", () => {
     });
 
     const ativoCadastra = await request(app).post("/ativo/cadastrar").send({
-      token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjYyMTIzODI3fQ.3lQBO3qx2fx2f7XjREYwnHnnuCwpjkd9d34ngS65s08",
+      token:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNjYzMTAyMDIxfQ.2-V1RYUu3Zlq9mmzR67YH_kmIGCMPdc-QJdxzcXC6fE",
       nomeAtivo: "ITAUSA",
-      preco: 30.00,
-      quantidade: 10,
+      preco: 30.0,
+      quantidade: 100,
       data: "02/02/2022",
       execucao: "compra",
-      sigla: "ITAU"
+      sigla: "ITAU",
     });
 
     const ativoCadastraErr = await request(app).post("/ativo/cadastrar").send({
-      token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaW",
+      token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNjYzMTAyMDIxfQ.2-V1RYUu3Zlq",
       nomeAtivo: "ITAUSA",
-      preco: 30.00,
-      quantidade: 10,
+      preco: 30.0,
+      quantidade: 20,
       data: "02/02/2022",
-      execucao: "venda",
-      sigla: "ITAU"
+      execucao: "compra",
+      sigla: "ITAU",
     });
 
   });
 
-  it('Vender um ativo', async () => {
+  it("Vender um ativo", async () => {
     const ativoVenda = await request(app).post("/ativo/vender").send({
-      token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjYyMTIzODI3fQ.3lQBO3qx2fx2f7XjREYwnHnnuCwpjkd9d34ngS65s08",
+      token:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNjYzMTAyMDIxfQ.2-V1RYUu3Zlq9mmzR67YH_kmIGCMPdc-QJdxzcXC6fE",
       nomeAtivo: "ITAUSA",
-      preco: 30.00,
+      preco: 30.0,
       quantidade: 10,
       data: "03/02/2022",
       execucao: "venda",
-      sigla: "ITAU"
+      sigla: "ITAU",
     });
 
     const ativoVenda2 = await request(app).post("/ativo/vender").send({
-      token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjYyMTIzODI3fQ.3lQBO3qx2fx2f7XjREYwnHnnuCwpjkd9d34ngS65s08",
+      token:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNjYzMTAyMDIxfQ.2-V1RYUu3Zlq9mmzR67YH_kmIGCMPdc-QJdxzcXC6fE",
       nomeAtivo: "ITAUSA",
-      preco: 30.00,
+      preco: 40.0,
       quantidade: 500,
       data: "03/02/2022",
       execucao: "venda",
-      sigla: "ITAU"
+      sigla: "ITAU",
     });
   });
 
-  it('Deve mostrar o historico', async () => {
-    const ativoHistorico = await request(app).get("/ativo/historico").send({
-      token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjYyMTIzODI3fQ.3lQBO3qx2fx2f7XjREYwnHnnuCwpjkd9d34ngS65s08"
+  it("Deve mostrar o historico", async () => {
+    const ativoHistorico = await request(app).post("/ativo/historico").send({
+      token:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNjYzMTAyMDIxfQ.2-V1RYUu3Zlq9mmzR67YH_kmIGCMPdc-QJdxzcXC6fE",
     });
     // expect(ativoHistorico.statusCode).toBe(200);
   });
 
-  it('Deve editar o ativo', () => {
-    
+  it("Deve editar o ativo", async () => {
+    const ativoEdita = await request(app).post("/ativo/editar").send({
+      token:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNjYzMTAyMDIxfQ.2-V1RYUu3Zlq9mmzR67YH_kmIGCMPdc-QJdxzcXC6fE",
+      sigla: "ITAU",
+      preco: 30.0,
+      quantidade: 2,
+    });
   });
 
+  it("Deve calcular o patrimonio", async () => {
+    const ativoPatrimonio = await request(app).post("/ativo/patrimonio").send({
+      token:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNjYzMTAyMDIxfQ.2-V1RYUu3Zlq9mmzR67YH_kmIGCMPdc-QJdxzcXC6fE",
+    });
+  });
+
+  it("Calcula rentabilidade", async () => {
+    const ativoRentabilidade = await request(app)
+      .post("/ativo/rentabilidade")
+      .send({
+        token:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNjYzMTAyMDIxfQ.2-V1RYUu3Zlq9mmzR67YH_kmIGCMPdc-QJdxzcXC6fE",
+      });
+  });
+
+  it("Deve buscar os ativos", async () => {
+    const ativoBusca = await request(app).get("/ativo/buscaativos");
+  });
+
+  it("Deve excluir um ativo", async () => {
+    const ativoExclui = await request(app).post("/ativo/excluir").send({
+      token:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNjYzMTAyMDIxfQ.2-V1RYUu3Zlq9mmzR67YH_kmIGCMPdc-QJdxzcXC6fE",
+    });
+  });
 });
--- Back/src/__tests__/usuario.test.js
+++ Back/src/__tests__/usuario.test.js
@@ -4,22 +4,23 @@ const app = require("../app");
 const db = require("../models/db");
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const AtivosB3 = require("../util/AtivosB3Util");
 
 describe("Testes de usuario", () => {
   beforeEach(async () => {
     console.log("Iniciando os Testes");
   });
-  // afterAll(async () => {
-  //   db.close();
-  // });
+  afterAll(async () => {
+    db.close();
+  });
   
   it("E possivel criar um novo usuario", async () => {
-
+    const i = 115;
     const novo_usuario = {
       nomeCompleto: "asd",
       dataNascimento: "02/02/2002",
       telefone: "61998753416",
-      email: "asd223@gmail.com",
+      email: "asd"+ i +"@gmail.com",
       senha: "123456789",
   }
   const novo_usuario2 = {
@@ -50,46 +51,29 @@ describe("Testes de usuario", () => {
 
     const usuario = await User.findOne({ where: { email: novo_usuario.email}  });
 
-    if (usuario === null){
-        await User.create(novo_usuario)
-        .then(() => {
-            return res.json({
-                erro: false,
-                message: "Usuario cadastrado com sucesso!"
-            }).expect.assertions(1)
-        }).catch((error) => {
-            expect(res.json.status).toBe(400);
-            // done(error);
-        });
-    }
-
   });
 
   it("E possivel Logar", async () => {
 
-    const usuario = await User.findOne({
-      attributes: ["id", "email", "senha"],
-      where: {
-          email: "asd7@gmail.com",
-         
-      }
-  });
-    const senha = await bcrypt.getSalt(usuario.senha);
-    const response2 = await request(app).post("/usuario/login").send({
+    const userLogin = await request(app).post("/usuario/login").send({
   
-      email: usuario.email,
-      senha: senha,
+      email: "teste1@gmail.com",
+      senha: "12345678",
     });
 
-    const usuario2 = null;
-    const senha2 = await bcrypt.getSalt(usuario.senha);
-    const response3 = await request(app).post("/usuario/login").send({
+    const userLoginErr = await request(app).post("/usuario/login").send({
   
-      email: usuario2,
-      senha: senha2,
+      email: "",
+      senha: "12345678",
+    });
+
+    const userLoginErr2 = await request(app).post("/usuario/login").send({
+  
+      email: "teste1@gmail.com",
+      senha: "12345678910",
     });
     
-
+    AtivosB3.updateAtivosB3();
   });
   
 });
