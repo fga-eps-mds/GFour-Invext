3980fb5c121bd1a3e51d61fcf011319b96aca01e
luciano-freitas-melo <202016847@aluno.unb.br>
2022-09-14 22:06:35 -0300

    refactor: gráfico da rentabilidade

--- Front/src/pages/Rentabilidade/rentabilidade.tsx
+++ Front/src/pages/Rentabilidade/rentabilidade.tsx
@@ -1,23 +1,10 @@
 import "./rentabilidade.css";
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
-import { VictoryChart, VictoryLabel, VictoryLine, VictoryTheme, VictoryZoomContainer } from 'victory';
+import { VictoryAxis, VictoryChart, VictoryLabel, VictoryLine, VictoryScatter, VictoryTheme, VictoryZoomContainer } from 'victory';
 import { useAuth } from "../../services/Provider";
 import { useEffect, useState } from "react";
 import Axios from "axios";
 import { format, parse } from "date-fns";
 import ptBR from 'date-fns/locale/pt-BR';
-
-const columns: GridColDef[] = [
-  { field: 'ativo', headerName: 'Ativo', width: 120 },
-  { field: 'sigla', headerName: 'Sigla', width: 120 },
-];
-
-const rows = [
-  { id: 1, ativo: 'Americanas', sigla: 'AMER3' },
-  { id: 2, ativo: 'OI', sigla: 'OIBR4' },
-  { id: 3, ativo: 'Petrobras', sigla: 'PETR4' },
-  { id: 4, ativo: 'Vale', sigla: 'VALE3' },
-];
 
 interface Rentabilidade {
   x: Date,
@@ -35,15 +22,17 @@ export const Rentabilidade = () => {
     Axios.post('/ativo/rentabilidade', {
       token: token
     }).then(function (response) {
+      console.log(response.data)
       setData(response.data.rentabilidade.map(
         (item: any) => ({
           x: parse(item.data, 'yyyy-MM', new Date(), { locale: ptBR }),
           y: Number(item.valor)
+
         }))
       );
 
     }).catch(function (error) {
-      console.log(error.message);
+      console.log(error);
     })
   }
 
@@ -61,18 +50,36 @@ export const Rentabilidade = () => {
             theme={VictoryTheme.material}
             width={900}
             height={420}
-            padding={{top: 20, bottom: 50, left: 70, right: 40}}
-            scale={{x:'time'}}
-            containerComponent={
-              <VictoryZoomContainer responsive={false}
-              />}
+            padding={{ top: 20, bottom: 50, left: 70, right: 40 }}
+            scale={{ x: 'time', y: 'linear' }}
           >
-            <VictoryLine
-              interpolation='linear'
+            <VictoryScatter
               data={data}
+            />
+
+            <VictoryLine
+              data={data}
+              interpolation='linear'
               style={{
                 data: { stroke: "#060b26" },
               }}
+            />
+
+            <VictoryAxis crossAxis
+              tickValues={data.map((item) => item.x)}
+              tickFormat={(x) => {
+                return format(x, "MMM/yyyy", { locale: ptBR });
+              }}
+            />
+
+            <VictoryAxis dependentAxis
+              label="Lucro / Total Gasto (%)"
+              // Os valores do eixo y são os mesmos de "data" + o valor 0
+              tickValues={[0].concat((data.map((item) => item.y)),[0])}
+              tickFormat={(y) => {
+                return `${y}%`
+              }}
+              style={{axisLabel: {padding: 50} }}
             />
           </VictoryChart>
         </div>
